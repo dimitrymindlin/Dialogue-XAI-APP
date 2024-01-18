d1fd3ad65a824dd76bd7fc6bf0d4405b7a8c5c7a
dimi9595 <dmitry.mindlin@gmail.com>
2024-01-18 11:02:54 +0100

    Removed torch typehints.

--- explain/mega_explainer/base_explainer.py
+++ explain/mega_explainer/base_explainer.py
@@ -1,9 +1,6 @@
 """The base explanation class"""
-import torch
-import torch.nn as nn
 
-
-class BaseExplainer(nn.Module):
+class BaseExplainer:
     """The base explanation class."""
 
     def __init__(self, model):
@@ -16,21 +13,21 @@ class BaseExplainer(nn.Module):
         self.model = model
 
     @staticmethod
-    def get_local_neighborhood(x: torch.Tensor) -> torch.Tensor:
+    def get_local_neighborhood(x):
         """
             Input : x : any given sample
             This function generates local neighbors of the input sample.
         """
 
-    def get_explanation(self, data_x: torch.FloatTensor, label) -> torch.FloatTensor:
+    def get_explanation(self, data_x, label):
         """
         Input : x : Input sample
         Output : This function uses the explanation model to return explanations for the given sample
         TODO : 1. If we are using public implementations of the explanation methods, do we require predict function?
         """
 
-    def evaluate_explanation(self, explanation: torch.Tensor, evaluation_metric: str,
-                             ground_truth: torch.Tensor = None) -> torch.Tensor:
+    def evaluate_explanation(self, explanation, evaluation_metric: str,
+                             ground_truth = None):
         """
         Input :-
         x : Input explanation for evaluation
