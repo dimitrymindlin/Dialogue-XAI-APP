67d4a8980fd4dd718d30eb274727f6a779089561
dimi9595 <dmitry.mindlin@gmail.com>
2024-01-31 15:01:12 +0100

    Added some allowance.

--- flask_app.py
+++ flask_app.py
@@ -237,8 +237,10 @@ if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 if __name__ == "__main__":
     # clean up storage file on restart
     app.logger.info(f"Launching app from config: {args.config}")
     app.run(debug=False, port=4455, host='0.0.0.0', use_reloader=False)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
