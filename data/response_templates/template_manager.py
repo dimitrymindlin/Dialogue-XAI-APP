124e6373d4d94800476119261d25ca4f396b70ad
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-20 09:13:50 +0100

    Added decode_numeric_columns_to_names and categorical_mapping.

--- data/response_templates/template_manager.py
+++ data/response_templates/template_manager.py
@@ -6,10 +6,12 @@ from data.response_templates.feature_display_names import FeatureDisplayNames
 class TemplateManager:
     def __init__(self,
                  conversation,
-                 encoded_col_mapping_path=None):
+                 encoded_col_mapping_path=None,
+                 categorical_mapping=None):
         self.conversation = conversation
         self.feature_display_names = FeatureDisplayNames(self.conversation)
         self.encoded_col_mapping = self._load_label_encoded_feature_dict(encoded_col_mapping_path)
+        self.categorical_mapping = categorical_mapping
 
     def _load_label_encoded_feature_dict(self, encoded_col_mapping_path):
         """
@@ -50,3 +52,56 @@ class TemplateManager:
         :return: display feature name
         """
         return self.feature_display_names.get_by_name(feature_name)
+
+    def decode_numeric_columns_to_names(self, df):
+        """
+        Function to turn integer dataframe to feature names
+        :param df: dataframe
+        :return: dataframe with feature names
+        """
+        if self.encoded_col_mapping is None:
+            return df
+        for col in df.columns:
+            if col in self.encoded_col_mapping:
+                df[col] = df[col].apply(lambda x: self.encoded_col_mapping[col].get(str(x), x))
+        return df
+
+    def apply_categorical_mapping(self, instances, is_dataframe=False):
+        """
+        Apply categorical mapping to instances.
+
+        Args:
+            instances (dict or DataFrame): The instances to apply categorical mapping on.
+            is_dataframe (bool): Flag to indicate if the instances are in a DataFrame. Default is False.
+
+        Returns:
+            The instances with applied categorical mapping.
+        """
+        if self.categorical_mapping is None:
+            if is_dataframe:
+                return instances.astype(float)
+            else:
+                return instances
+
+        if is_dataframe:
+            # Iterate only over columns that have a categorical mapping.
+            for column_index, column_mapping in self.categorical_mapping.items():
+                column_name = instances.columns[column_index]
+                old_values_copy = int(instances[column_name].values)
+                if column_name in instances.columns:
+                    # Prepare a mapping dictionary for the current column.
+                    mapping_dict = {i: category for i, category in enumerate(column_mapping)}
+                    # Replace the entire column values based on mapping_dict.
+                    instances[column_name] = instances[column_name].replace(mapping_dict)
+                    if old_values_copy == instances[column_name].values[0]:
+                        raise ValueError(f"Column {column_name} was not replaced with categorical mapping.")
+        else:
+            for i, (feature_name, val) in enumerate(instances.items()):
+                index_as_str = str(i)
+                if index_as_str in self.categorical_mapping:
+                    try:
+                        instances[feature_name] = self.categorical_mapping[index_as_str][val]
+                    except KeyError:
+                        raise ValueError(f"Value {val} not found in categorical mapping for feature {feature_name}.")
+
+        return instances
