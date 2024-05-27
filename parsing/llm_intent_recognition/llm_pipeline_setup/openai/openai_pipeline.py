b6aa829d7793a0dee9eab0b1db86e812c63af8ac
dimi9595 <dmitry.mindlin@gmail.com>
2024-05-27 13:28:02 +0200

    streamlined script.

--- parsing/llm_intent_recognition/llm_pipeline_setup/openai/openai_pipeline.py
+++ parsing/llm_intent_recognition/llm_pipeline_setup/openai/openai_pipeline.py
@@ -1,36 +1,20 @@
 import json
-import re
-import sys
 
 import pandas as pd
-from langchain.chains.llm import LLMChain
-from langchain.chains.router import MultiPromptChain
-from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
-from langchain_core.messages import AIMessage
-from langchain.chains import ConversationChain
-from langchain_core.pydantic_v1 import BaseModel, Field
 
-from langchain_core.output_parsers import JsonOutputParser
 from openai import OpenAI
 from openai.types.chat.completion_create_params import ResponseFormat
 
-from langchain.chat_models import ChatOpenAI
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
-from difflib import SequenceMatcher
+from langchain_core.prompts import ChatPromptTemplate
 import tqdm
 import matplotlib.pyplot as plt
 
 import os
 from dotenv import load_dotenv
 
-from parsing.llm_intent_recognition.prompts import get_template_with_full_descriptions, \
-    get_xai_template_with_descriptions, \
-    possible_categories, question_to_id_mapping, \
-    get_template_with_full_descr_step_by_step, get_template_wich_checklist, \
-    get_tempalte_wich_checklist_and_memory, get_template_with_checklist_condensed, get_system_template_with_checklist, \
-    simple_user_question_prompt, get_system_prompt_condensed, openai_system_prompt, openai_user_prompt
-from parsing.llm_intent_recognition.router_chain import generate_destination_chains, generate_router_chain
+from parsing.llm_intent_recognition.prompts import question_to_id_mapping, openai_system_prompt, openai_user_prompt
 from langchain_community.adapters import openai as lc_openai
 
 load_dotenv()
@@ -44,9 +28,9 @@ os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
 os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 os.environ["OPENAI_ORGANIZATION"] = os.getenv('OPENAI_ORGANIZATION_ID')
 
-LLM_MODEL = "gpt-3.5-turbo-0125"
+LLM_MODEL = os.getenv('OPENAI_MODEL_NAME')
 client = OpenAI()
-llm = ChatOpenAI(model=LLM_MODEL, temperature=0.0)
+# llm = ChatOpenAI(model=LLM_MODEL, temperature=0.0)
 
 response_schemas = [
     ResponseSchema(name="method_name", description="name of the method to answer the user question."),
@@ -55,102 +39,6 @@ response_schemas = [
 
 output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 format_instructions = output_parser.get_format_instructions()
-
-
-def extract_json_from_response(response) -> dict:
-    """
-    Extracts JSON object from a response string that contains both justification and JSON formatted within triple backticks.
-
-    Parameters:
-        response (str): The response string from which to extract the JSON.
-
-    Returns:
-        dict: The extracted JSON object.
-    """
-    # Use regular expression to find the JSON object within triple backticks
-    try:
-        json_obj = json.loads(response)
-        return json_obj
-    except json.JSONDecodeError:
-        json_match = re.search(r'```(.*?)```', response.content, re.DOTALL)
-        if json_match:
-            json_str = json_match.group(1).strip()
-            try:
-                json_obj = json.loads(json_str)
-                return json_obj
-            except json.JSONDecodeError:
-                raise ValueError("Found JSON block, but it is not valid JSON.")
-
-    # If no JSON object is found, raise an error
-    raise ValueError("No JSON block found in the response.")
-
-
-def extract_tuple_from_response(response) -> tuple:
-    """
-    Extracts a Python tuple from a response string that contains both justification and tuple formatted within triple backticks.
-
-    Parameters:
-        response (str): The response string from which to extract the tuple.
-
-    Returns:
-        tuple: The extracted Python tuple.
-    """
-    # Use regular expression to find the tuple within triple backticks
-    if isinstance(response, dict):
-        answer = response['text']
-    elif isinstance(response, AIMessage):
-        answer = response.content
-    else:  # str
-        answer = response
-
-    tuple_match = re.search(r'\(([^)]+)\)', answer)
-
-    if tuple_match:
-        tuple_str = tuple_match.group(1).strip()
-        try:
-            # Evaluate the string as a Python tuple
-            tuple_obj = eval(tuple_str)
-            if isinstance(tuple_obj, tuple):
-                return tuple_obj
-            else:
-                print(answer)
-                raise ValueError("Found block, but it is not a valid tuple.")
-        except (SyntaxError, NameError):
-            print(answer)
-            raise ValueError("Found block, but it is not a valid tuple.")
-
-    # If no tuple is found, raise an error
-    raise ValueError("No tuple block found in the response.")
-
-
-"""chain = (
-        PromptTemplate.from_template(
-            template=get_tempalte_wich_checklist(),
-            partial_variables={"input": "{question}"}) | Ollama(model=LLM_MODEL,
-                                                                    temperature=0) | extract_tuple_from_response
-)"""
-
-
-class LLMSinglePromptSystemMessage:
-    def __init__(self):
-        self.system_prompt = get_system_prompt_condensed()
-        self.chain = (
-                PromptTemplate.from_template(
-                    template=simple_user_question_prompt(),
-                    partial_variables={"input": "{question}"}) | llm | extract_json_from_response
-        )
-
-    def predict(self, question):
-        message = self.chain.invoke({"input": f"{question}"})
-        if isinstance(message, dict):
-            question = message["method"]
-            feature = message["feature"]
-        else:
-            question = message[0]
-            feature = message[1]
-        # get ids
-        question_id = question_to_id_mapping[question]
-        return question_id, feature
 
 
 class LLMSinglePromptWithMemoryAndSystemMessage:
@@ -213,109 +101,6 @@ class LLMSinglePromptWithMemoryAndSystemMessage:
             feature = response["feature"]
         question_id = question_to_id_mapping.get(mapped_question, -1)
         return question_id, feature
-
-
-class LLMMultiPromptChainModel:
-    def __init__(self):
-        self.chain = multi_prompt_chain
-
-    def predict(self, question):
-        response = self.chain.invoke({"input": question})
-        response["text"] = response["text"].replace("\n", "")
-        response["text"] = response["text"].strip()
-        response = json.loads(response["text"])
-        method_name = response.get("method_name")
-        try:
-            feature = response.get("feature")
-        except KeyError:
-            feature = None
-        question_id = question_to_id_mapping.get(method_name)
-        return question_id, feature
-
-
-class LLMMultiChain:
-    def __init__(self):
-        self.chain = None
-        self.build_chain()
-
-    def build_chain(self):
-        prompt_infos, destination_chains, default_chain = generate_destination_chains()
-        self.chain = generate_router_chain(prompt_infos, destination_chains, default_chain)
-
-    def predict(self, question):
-        message = self.chain.invoke({"input": f"{question}"})
-        if not isinstance(message["text"], dict):
-            answer_dict = json.loads(message["text"])
-        else:
-            answer_dict = message["text"]
-        question = answer_dict["method_name"]
-        feature = answer_dict["feature"]
-        # get ids
-        question_id = question_to_id_mapping[question]
-        return question_id, feature
-
-
-# Define prompt names and descriptions
-prompt_names = [
-    "xai_method", "whyExplanation", "notXaiMethod", "followUp", "greeting"
-]
-
-prompt_descriptions = [
-    "Prompt to select the most appropriate explainability method for the user's question about the model prediction. Useful when the user seeks explanations that can be answered by XAI methods like anchors (sufficient conditions), SHAP values (feature importances), counterfactuals (alternative scenarios), or feature statistics (data analysis).",
-    "Prompt to address general 'why did this happen' questions related to model predictions. Suitable for inquiries that require a straightforward explanation without delving into specific alternative scenarios or feature changes.",
-    "Prompt to respond to user questions unrelated to model predictions or feature specifics, which cannot be addressed using standard XAI methods.",
-    "Prompt to handle follow-up questions that reference previous interactions, especially when the user shifts focus to a different feature or aspect. Useful for maintaining context continuity.",
-    "Prompt to manage greetings and general inquiries not directly tied to the model predictions or specific features, ensuring a friendly and informative interaction."
-]
-
-# Define the templates for each prompt
-xai_method_template = get_xai_template_with_descriptions()
-
-why_template = """Respond with the following json: {"method_name": "whyExplanation"}"""
-
-greeting_template = """Respond with the following json: {"method_name": "greeting"}"""
-
-notXaiMethod_template = """Respond with the following json: {"method_name": "notXaiMethod"}"""
-
-followUp_template = """Respond with the following json: {"method_name": "followUp"}"""
-
-# Collect all templates in a list
-prompt_templates = [
-    xai_method_template, why_template, notXaiMethod_template, followUp_template, greeting_template
-]
-
-# Create prompt_infos dict with "name" and "description" and "prompt_template" keys
-prompt_infos = [
-    {"name": name, "description": description, "prompt_template": template}
-    for name, description, template in zip(prompt_names, prompt_descriptions, prompt_templates)
-]
-
-# Initialize the MultiPromptChain with the LLM and the prompts
-multi_prompt_chain = MultiPromptChain.from_prompts(
-    llm,
-    prompt_infos=prompt_infos,
-)
-
-
-def parse_categorical(ai_message: AIMessage) -> str:
-    """
-    Parse the AI output to identify one of the possible categories. This includes checking for exact matches
-    and very close matches that account for potential misspellings or minor variations in the text.
-    """
-    message_content = ai_message.content.lower()
-    question = None
-
-    for category in possible_categories:
-        if category in message_content:
-            question = category
-        # Check for very close matches using a similarity threshold
-        elif SequenceMatcher(None, category, message_content).ratio() > 0.9:
-            question = category
-
-    # get question id
-    question_id = question_to_id_mapping[question]
-
-    return question_id
 
 
 def current_approach_performance(question_to_id_mapping, load_previous_results=False):
