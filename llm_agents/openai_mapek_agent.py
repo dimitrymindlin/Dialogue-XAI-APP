885f1fbe013d7bc6833fe21ff62dd538bcd0603b
dimitrymindlin <dmitry.mindlin@gmail.com>
2025-07-16 09:40:18 +0200

    Refactored local logging and logging_experiment_id.

--- llm_agents/openai_mapek_agent.py
+++ llm_agents/openai_mapek_agent.py
@@ -633,7 +633,7 @@ class MapeKUnifiedOpenAIAgent(OpenAIAgent):
             # Log to CSV
             row = {
                 "timestamp": datetime.now().strftime("%d.%m.%Y_%H:%M"),
-                "experiment_id": self.experiment_id,
+                "experiment_id": self.logging_experiment_id,
                 "datapoint_count": self.datapoint_count,
                 "user_message": user_question,
                 "monitor": {"mode_of_engagement": unified_res.final_output.mode_of_engagement,
